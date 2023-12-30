efadd3c954210601283c927e02712850af46c6fc
ROSORRNT <45917975+ROSORRNT@users.noreply.github.com>
2023-12-30 09:19:10 +0100

    git commit -m "Ajout de données de test pour l'application"

---
+++ src/data.js
@@ -0,0 +1,34 @@
+// data.js
+const postsData = [
+  {
+    id: 1,
+    imageUrl: "../public/2021-12-21_07-34.jpg",
+    paragraphs: [
+      "Lorsque la Nature chuchote à mon oreille ses sublimes poèmes, la volonté d'arrêter le temps est une grande tentation. Je sors alors mon appareil photo (téléphone, peu importe) et dans un geste, sacrilège lorsqu'il se fait sans conscience, sans attention, donc sans amour, j'essaie de saisir la beauté.",
+      "Mais en ces instants, c'est bien l'Amour qui m'accueille. Je dois lui rendre hommage, lui rendre grâce, le prier, et m'arrêter dans une attention claire (le geste pouvant venir ensuite). Et il n'y a que l'Amour qui puisse arrêter la main sacrilège, et remettre le temps dans son axe: celui du cœur.",
+      "C'est alors qu'une étrange sensation d'irréalité me rend au réel. Une sensation de légèreté. Ce réel si souvent oublié dans nos douloureuses et infinies chutes.",
+      "Ce réel: la certitude qu'à tout moment je peux être autre, et partir n'importe où, puisque mon cœur et le temps seront enfin alignés; la certitude d'être libre: une liberté archaïque, atavique même, comme ces plumes que je crois sentir sur mes pieds et sur mes poignets."
+    ],
+  },
+  {
+    id: 2,
+    imageUrl: "../public/2021-12-21_07-34.jpg",
+    paragraphs: [
+      "Lorsque la Nature chuchote à mon oreille ses sublimes poèmes, la volonté d'arrêter le temps est une grande tentation. Je sors alors mon appareil photo (téléphone, peu importe) et dans un geste, sacrilège lorsqu'il se fait sans conscience, sans attention, donc sans amour, j'essaie de saisir la beauté.",
+      "Mais en ces instants, c'est bien l'Amour qui m'accueille. Je dois lui rendre hommage, lui rendre grâce, le prier, et m'arrêter dans une attention claire (le geste pouvant venir ensuite). Et il n'y a que l'Amour qui puisse arrêter la main sacrilège, et remettre le temps dans son axe: celui du cœur.",
+      "C'est alors qu'une étrange sensation d'irréalité me rend au réel. Une sensation de légèreté. Ce réel si souvent oublié dans nos douloureuses et infinies chutes.",
+      "Ce réel: la certitude qu'à tout moment je peux être autre, et partir n'importe où, puisque mon cœur et le temps seront enfin alignés; la certitude d'être libre: une liberté archaïque, atavique même, comme ces plumes que je crois sentir sur mes pieds et sur mes poignets."
+    ],
+  },
+  // {
+  //   id: 2,
+  //   imageUrl: "/path/to/your/second/image.jpg",
+  //   paragraphs: [
+  //     // ...paragraphes pour le deuxième post
+  //   ],
+  // },
+  // ...plus de posts
+];
+
+export default postsData;
+
